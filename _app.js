e9488acc93c5178e0d0756f0dcd7e56ea3bc8538
Ben <nyatomeb@jacks.sfasu.edu>
2025-07-02 14:43:24 -0500

    Add files via upload

--- _app.js
+++ _app.js
@@ -1,6 +1,29 @@
-import '../styles/globals.css'
+import 'bootstrap/dist/css/bootstrap.min.css'
+import Link from 'next/link'
 
 export default function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
-}
+  return (
+    <>
+      {/* ✅ Navbar */}
+      <nav className="navbar navbar-expand-lg navbar-dark bg-dark px-4">
+        <Link href="/" className="navbar-brand fw-bold text-warning">
+          📚 Straw Hat Reviews
+        </Link>
 
+        <div className="collapse navbar-collapse">
+          <ul className="navbar-nav ms-auto">
+            <li className="nav-item">
+              <Link href="/" className="nav-link">Home</Link>
+            </li>
+            <li className="nav-item">
+              <Link href="/add" className="nav-link">Add Book</Link>
+            </li>
+          </ul>
+        </div>
+      </nav>
+
+      {/* ✅ Page Content */}
+      <Component {...pageProps} />
+    </>
+  )
+}
